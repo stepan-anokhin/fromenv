211183c12b4b3287d32e9ff934daab97b5b0e546
Stepan Anokhin <stepan.anokhin@gmail.com>
2024-04-26 13:14:20 +0700

    Specify exported symbols by the root module

--- fromenv/__init__.py
+++ fromenv/__init__.py
@@ -1,11 +1,18 @@
 import os
-from dataclasses import dataclass
 from typing import Type, Dict, TypeVar
 
 from fromenv.errors import MissingRequiredVar, AmbiguousVarError
 from fromenv.internal.data_classes import DataClasses
 from fromenv.internal.dicts import Dicts
 from fromenv.internal.value import Config, Value, Strategy, VarBinding
+
+# Specify exported symbols
+__all__ = (
+    "from_env",
+    "Config",
+    "MissingRequiredVar",
+    "AmbiguousVarError",
+)
 
 T = TypeVar("T")
 
@@ -22,23 +29,3 @@ def from_env(data_class: Type[T], env: Dict[str, str] | None = None, config: Con
     root_value = strategy.root_value(data_class)
     loader = strategy.resolve_loader(root_value)
     return loader.load(VarBinding(env), root_value, strategy)
-
-
-@dataclass
-class Server:
-    host: str
-    port: str
-    retries: int
-
-
-@dataclass
-class ServiceConfig:
-    secret_path: str
-    server: Server
-    optional_property: int = 10
-
-
-if __name__ == "__main__":
-    cfg = from_env(ServiceConfig, {"SECRET_PATH": "/some/path", "SERVER_HOST": "example.com", "SERVER_PORT": "8080",
-                                   "SERVER_RETRIES": "10"})
-    print(cfg)
