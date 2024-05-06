86feb21675ea829ca86e11caf841cb0e7d329640
Stepan Anokhin <stepan.anokhin@gmail.com>
2024-05-06 23:07:44 +0700

    Add tests

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -22,3 +22,64 @@ which default value should be produced:
 This test-module checks if the above convention is correctly
 implemented.
 """
+
+from dataclasses import dataclass, field
+from typing import List, Tuple
+
+from fromenv import from_env
+
+
+def test_type_specific_default_for_fields():
+    """Check default value for each data type."""
+
+    @dataclass
+    class Nested:
+        """Class without required fields."""
+
+        value: int = 0
+
+    @dataclass
+    class TestData:
+        """No field-specific defaults."""
+
+        nested: Nested
+        list: List[str]
+        tuple: Tuple[str, ...]
+        fixed_tuple: Tuple[Nested, Nested]
+        optional: str | None
+
+    assert from_env(TestData, {}) == TestData(
+        nested=Nested(),
+        list=[],
+        tuple=(),
+        fixed_tuple=(Nested(), Nested()),
+        optional=None,
+    )
+
+
+def test_field_specific_default_instead_of_type_specific():
+    """Check that field-specific defaults overrides type-specific ones."""
+
+    @dataclass(frozen=True)
+    class Nested:
+        """Class without required fields."""
+
+        value: str = "type-specific"
+
+    @dataclass
+    class TestData:
+        """No field-specific defaults."""
+
+        nested: Nested = Nested("field-specific")
+        list: List[str] = field(default_factory=lambda: ["field-specific"])
+        tuple: Tuple[str, ...] = ("field-specific",)
+        fixed_tuple: Tuple[Nested, Nested] = (Nested("field"), Nested("specific"))
+        optional: str | None = "field-specific"
+
+    assert from_env(TestData, {}) == TestData(
+        nested=Nested("field-specific"),
+        list=["field-specific"],
+        tuple=("field-specific",),
+        fixed_tuple=(Nested("field"), Nested("specific")),
+        optional="field-specific",
+    )
